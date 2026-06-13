9244b2d61648cea760a05adbe4c4bb2c298779aa
NickelSympa <124716497+NickelSympa@users.noreply.github.com>
2026-06-13 12:04:36 +0200

    Update accounts.js

--- accounts.js
+++ accounts.js
@@ -4,8 +4,8 @@ const ASIA_ACCOUNTS = {
     { username: "superviseur67", password: "secteurd67",    name: "Superviseur Site 67", role: "Superviseur Site 67",   clearance: 3, permissions: "Dossiers personnel, supervision consultations" },
     { username: "direction67",   password: "o5site67",      name: "Direction Site 67",  role: "Direction Site 67",      clearance: 4, permissions: "Acces complet archives classifiees" },
     { username: "archiviste",    password: "archives",      name: "Archiviste",         role: "Archiviste ASIA",        clearance: 3, permissions: "Gestion archives niveau 3" },
-    { username: "agent_l4",      password: "asia",          name: "Agent Niveau 4",     role: "Agent Special",          clearance: 4, permissions: "Acces zones restreintes" },
-    { username: "directeur",     password: "asia",          name: "Directeur ASIA",     role: "Directeur",              clearance: 5, permissions: "Acces total, override securite" },
+    { username: "agent_l4",      password: "asial4",          name: "Agent Niveau 4",     role: "Agent Special",          clearance: 4, permissions: "Acces zones restreintes" },
+    { username: "directeur",     password: "asiadirecteur",          name: "Directeur ASIA",     role: "Directeur",              clearance: 5, permissions: "Acces total, override securite" },
     { username: "chercheur",     password: "labo",          name: "Chercheur",          role: "Chercheur SCP",          clearance: 3, permissions: "Laboratoires, protocoles" },
     { username: "asia_admin",    password: "secure_admin",  name: "Admin ASIA",         role: "Administrateur ASIA",    clearance: 5, permissions: "Super administrateur" }
   ],
